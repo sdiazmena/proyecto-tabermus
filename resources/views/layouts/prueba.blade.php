b8f0050be5ab48f809cd591509df0d625d24bda5
in-yourface <daffyluis@hotmail.com>
2017-10-24 13:34:22 -0300

    ultima prueba

--- resources/views/layouts/prueba.blade.php
+++ resources/views/layouts/prueba.blade.php
@@ -114,7 +114,7 @@
                                 <ul class="dropdown-menu" role="menu">
 
                                 @if(Auth::user()->password == '')
-                                    <img src="{{Auth::user()->avatar}}" style="width:75px; height:75px; float:center; border-radius:50%; ">
+                                    <img src="{{Auth::user()->avatar}}" style="width:75px; height:75px; float:center; border-radius:50%; "> 
                                 @else
                                     <img src="uploads/avatars/{{ Auth::user()->avatar }}" style="width:75px; height:75px; float:center; border-radius:50%; margin-right:25px;">
 
