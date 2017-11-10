42a9cd4a66958e82a8590e4442e43196dcb50b81
in-yourface <dafftyluis@hotmail.com>
2017-11-09 22:56:21 -0300

    asd

--- resources/views/layouts/prueba.blade.php
+++ resources/views/layouts/prueba.blade.php
@@ -144,6 +144,7 @@
                                             <span class="glyphicon glyphicon-log-out"></span>Logout
                                         </a>
 
+                                        
                                         <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
                                             {{ csrf_field() }}
                                         </form>
