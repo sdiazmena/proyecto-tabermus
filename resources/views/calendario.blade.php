2ad9b9d084a79bf5b6911a8402cc300e67800487
in-yourface <dafftyluis@hotmail.com>
2017-11-24 01:21:09 -0300

    vista de los eventon con modal en el calendario

--- resources/views/calendario.blade.php
+++ resources/views/calendario.blade.php
@@ -30,13 +30,52 @@
 			<div id="calendar" class="fondoContenido"></div>
 
     </div>
+
+	<div id="responsive-modal" class="modal fade" tabindex="-1" data-backdrop="static">
+		<div class="modal-dialog">
+			<div class="modal-content fondoContenido letraTitulo">
+				<div class="modal-header">
+					<h4 color="black" class="titulo">Informacion Show</h4>
+				</div>
+				<div class="modal-body">
+					<div class="row">
+						<strong>Nombre:</strong>
+						<span id="nombreShow"></span>
+					</div>
+					<div class="row">
+						<strong>Ciudad:</strong>
+						<span id="ciudadShow"></span>
+					</div>
+					<div class="row">
+						<strong>Fecha/Hora Inicio:</strong>
+						<span id="fechaShow"></span>
+					</div>
+					<div class="row">
+						<strong>Informacion:</strong>
+						<span id="infShow"></span>
+					</div>
+					<div class="row">
+						<strong>Valor:</strong>
+						<span id="valor"></span>
+					</div>
+					<div class="row">
+						<strong>Mas Informacion visitar:</strong>
+						<span id="facebook"></span>
+					</div>
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-dafault letraPortada" data-dismiss="modal">Salir</button>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
 
 @section('scripts')
     {{ Html::script('calendar/fullcalendar/lib/jquery-ui.min.js') }}
     {{ Html::script('calendar/fullcalendar/lib/moment.min.js') }}
     {{ Html::script('calendar/fullcalendar/fullcalendar.min.js') }}
-    
+
     {{ Html::script('calendar/fullcalendar/locale/es.js') }}
     <script>
         //inicializamos el calendario al cargar la pagina
@@ -44,7 +83,7 @@
 $(document).ready(function() {
 		
 		$('#calendar').fullCalendar({
-			header: {
+            header: {
 				left: 'prev,next today',
 				center: 'title',
 				right: 'month,basicWeek,basicDay'
@@ -52,7 +91,35 @@ $(document).ready(function() {
 			navLinks: true, // can click day/week names to navigate views
 			editable: true,
 			eventLimit: true, // allow "more" link when too many events
-			events: BASEURL+'/calendarios'
+			events: BASEURL+'/calendarios',
+
+            eventClick: function(calEvent, jsEvent, view) {
+
+                //$(this).css('background', 'red');
+                $('#responsive-modal').modal('show');
+                var BASEURL = '{{ url('/') }}';
+
+                //alert('Event: ' + calEvent.id);
+                //al evento click; al hacer clic sobre un evento cambiara de background
+                //a color rojo y nos enviara a los datos generales del evento seleccionado
+
+                $.ajax({
+                    url: BASEURL+'/show/'+calEvent.id ,
+                    type : 'GET',
+                    success: function(result){
+                        console.log(result);
+                        $('#nombreShow').text(result[0].title);
+                        $('#ciudadShow').text(result[0].id_ciudad);
+                        $('#fechaShow').text(result[0].start);
+                        $('#infShow').text(result[0].informacion);
+                        $('#valor').text(result[0].precio);
+                        $('#facebook').text(result[0].link);
+                    },
+                    error: function(){
+                        console.log('Error:');
+                    }
+                })
+            },
 		});
 		
 	});
