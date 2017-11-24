8d26b9df8b8c89a140ef8118200847624d119501
in-yourface <dafftyluis@hotmail.com>
2017-11-24 02:41:37 -0300

    ahora redige bien

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -17,41 +17,7 @@ class HomeController extends Controller
         $valorImprimir = "img/region/portada".$valor.".jpg";
         $valorImprimir = "<img id='portada1' class='img-responsive ' src=".$valorImprimir." class='img-thumbnail' style='width:820px; height: 350px'>";
 
-        //dd($valorImprimir);
-
-        $nombreRegion =  $_COOKIE["nameRegion"];
-        if($nombreRegion == "O'Higgins"){
-            $nombreRegion = "Región del Libertador Gral. Bernardo O’Higgins";
-        }
-        if($nombreRegion == 'Maule'){
-            $nombreRegion = "Región del Maule";
-        }   
-        if($nombreRegion == 'Biobío'){
-            $nombreRegion = "Región del Biobío";
-        }
-        if($nombreRegion == 'Araucania'){
-            $nombreRegion = "Región de la Araucanía";
-        }
-        if($nombreRegion == 'Los Lagos'){
-            $nombreRegion = "Región de Los Lagos";
-        }
-        if($nombreRegion == 'Aisén'){
-            $nombreRegion = "Región Aisén del Gral. Carlos Ibáñez del Campo";
-        }
-        if($nombreRegion == 'Magallanes'){
-            $nombreRegion = "Región de Magallanes y de la Antártica Chilena";
-        }
-        if($nombreRegion == 'Santiago'){
-            $nombreRegion = "Región Metropolitana de Santiago";
-        }
-        if($nombreRegion == 'Los Rios'){
-            $nombreRegion = "Región de Los Ríos";
-        }
-        if($nombreRegion == 'Arica'){
-            $nombreRegion = "Arica y Parinacota";
-        }
-    	$id = DB::table('region')->where('nombre',$nombreRegion)->get(['id']);
-    	$data = DB::table('Actualizacion')->where('id_region',$id[0]->id)->orderBy('id', 'desc')->get();
+    	$data = DB::table('Actualizacion')->where('id_region',$valor)->orderBy('id', 'desc')->get();
     	$bandas = DB::table('banda')->get();
         //dd($data);
         return view('welcome')->with('actualizaciones',$data)->with('bandas',$bandas)->with('foto',$valorImprimir);
--- public/js/scriptRegionesBarraMenu.js
+++ public/js/scriptRegionesBarraMenu.js
@@ -4,48 +4,48 @@ function actualizarBarraRegion(valor) {
     $("#img2").empty();
     //$('#img2').load("js/imagenes.html #portada"+id);
     if(!(valor === "1")){
-        $("#barraRegionesTop").append("<li><a href=\"home\" onclick=\"setRegion('1','Tarapacá')\">Tarapacá</a></li>");
+        $("#barraRegionesTop").append("<li><a href='/tabermus/public/home' onclick=\"setRegion('1','Tarapacá')\">Tarapacá</a></li>");
     }
     if(!(valor === "2")){
-        $("#barraRegionesTop").append("<li><a href=\"home\" onclick=\"setRegion('2','Antofagasta')\">Antofagasta</a></li>");
+        $("#barraRegionesTop").append("<li><a href='/tabermus/public/home' onclick=\"setRegion('2','Antofagasta')\">Antofagasta</a></li>");
     }
     if(!(valor === "3")){
-        $("#barraRegionesTop").append("<li><a href=\"home\" onclick=\"setRegion('3','Atacama')\">Atacama</a></li>");
+        $("#barraRegionesTop").append("<li><a href='/tabermus/public/home' onclick=\"setRegion('3','Atacama')\">Atacama</a></li>");
     }
     if(!(valor === "4")){
-        $("#barraRegionesTop").append("<li><a href=\"home\" onclick=\"setRegion('4','Coquimbo')\">Coquimbo</a></li>");
+        $("#barraRegionesTop").append("<li><a href='/tabermus/public/home' onclick=\"setRegion('4','Coquimbo')\">Coquimbo</a></li>");
     }
     if(!(valor === "5")){
-        $("#barraRegionesTop").append("<li><a href=\"home\" onclick=\"setRegion('5','Valparaiso')\">Valparaiso</a></li>");
+        $("#barraRegionesTop").append("<li><a href='/tabermus/public/home' onclick=\"setRegion('5','Valparaiso')\">Valparaiso</a></li>");
     }
     if(!(valor === "6")){
-        $("#barraRegionesTop").append("<li><a href=\"home\" onclick=\"setRegion('6','O\\'Higgins')\">O'Higgins</a></li>");
+        $("#barraRegionesTop").append("<li><a href='/tabermus/public/home' onclick=\"setRegion('6','O\\'Higgins')\">O'Higgins</a></li>");
     }
     if(!(valor === "7")){
-        $("#barraRegionesTop").append("<li><a href=\"home\" onclick=\"setRegion('7','Maule')\">Maule</a></li>");
+        $("#barraRegionesTop").append("<li><a href='/tabermus/public/home' onclick=\"setRegion('7','Maule')\">Maule</a></li>");
     }
     if(!(valor === "8")){
-        $("#barraRegionesTop").append("<li><a href=\"home\" onclick=\"setRegion('8','Biobío')\">Biobío</a></li>");
+        $("#barraRegionesTop").append("<li><a href='/tabermus/public/home' onclick=\"setRegion('8','Biobío')\">Biobío</a></li>");
     }
     if(!(valor === "9")){
-        $("#barraRegionesTop").append("<li><a href=\"home\" onclick=\"setRegion('9','Araucania')\">Araucania</a></li>");
+        $("#barraRegionesTop").append("<li><a href='/tabermus/public/home' onclick=\"setRegion('9','Araucania')\">Araucania</a></li>");
     }
     if(!(valor === "10")){
-        $("#barraRegionesTop").append("<li><a href=\"home\" onclick=\"setRegion('10','Los Lagos')\">Los Lagos</a></li>");
+        $("#barraRegionesTop").append("<li><a href='/tabermus/public/home' onclick=\"setRegion('10','Los Lagos')\">Los Lagos</a></li>");
     }
     if(!(valor === "11")){
-        $("#barraRegionesTop").append("<li><a href=\"home\" onclick=\"setRegion('11','Aisén')\">Aisén</a></li>");
+        $("#barraRegionesTop").append("<li><a href='/tabermus/public/home' onclick=\"setRegion('11','Aisén')\">Aisén</a></li>");
     }
     if(!(valor === "12")){
-        $("#barraRegionesTop").append("<li><a href=\"home\" onclick=\"setRegion('12','Magallanes')\">Magallanes</a></li>");
+        $("#barraRegionesTop").append("<li><a href='/tabermus/public/home' onclick=\"setRegion('12','Magallanes')\">Magallanes</a></li>");
     }
     if(!(valor === "13")){
-        $("#barraRegionesTop").append("<li><a href=\"home\" onclick=\"setRegion('13','Santiago')\">Santiago</a></li>");
+        $("#barraRegionesTop").append("<li><a href='/tabermus/public/home' onclick=\"setRegion('13','Santiago')\">Santiago</a></li>");
     }
     if(!(valor === "14")){
-        $("#barraRegionesTop").append("<li><a href=\"home\" onclick=\"setRegion('14','Los Rios')\">Los Rios</a></li>");
+        $("#barraRegionesTop").append("<li><a href='/tabermus/public/home' onclick=\"setRegion('14','Los Rios')\">Los Rios</a></li>");
     }
     if(!(valor === "15")){
-        $("#barraRegionesTop").append("<li><a href=\"home\" onclick=\"setRegion('15','Arica')\">Arica</a></li>");
+        $("#barraRegionesTop").append("<li><a href='/tabermus/public/home' onclick=\"setRegion('15','Arica')\">Arica</a></li>");
     }
 }
